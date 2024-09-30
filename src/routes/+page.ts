542fbc9c6062000de1f4868e30e779299b0b7be1
Nik <niklas@kizo-agency.com>
2024-09-30 10:40:39 +0200

    2_16 - Process raw data from sanity

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,12 +1,22 @@
-import type { DevExperience } from "$lib/types/sanity";
-import sanityClient from "$lib/utils/sanity";
+import sanityClient, { processProjectEntries } from "$lib/utils/sanity";
 import type { PageLoad } from "./$types";
 
 export const load: PageLoad = async () => {
-  const workExperience: DevExperience[] = await sanityClient.fetch(
+  const workExperience: SanityWorkExperience[] = await sanityClient.fetch(
     '*[_type == "devExperience"] | order(startDate desc)'
   );
 
+  const rawProjects: SanityProject[] = await sanityClient.fetch(
+    "*[_type == 'project']"
+  );
+
+  console.log("BEFORE TRANSFORMATION");
+  console.log(rawProjects[0]);
+
+  const projects = rawProjects.map(processProjectEntries);
+
+  console.log("AFTER THE TRANSFORMATION");
+  console.log(projects[0]);
   return {
     workExperience,
   };
